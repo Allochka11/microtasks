2781d6456c6911a81cfadc86515e584bd0c96834
Allochka11 <allavelychko11@gmail.com>
2022-07-19 15:06:23 +0300

    adding topCars mapping in a table

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,20 @@ import './App.css';
 import {Header} from "./site/Header";
 import {Body} from "./site/Body";
 import {Footer} from "./site/Footer";
+import {NewComponent} from "./Microtask2/NewComponent";
 
 function App() {
+    let students = [
+        {id: 1, name: 'Alla', age: 30},
+        {id: 2, name: 'Vladymyr', age: 31},
+        {id: 3, name: 'Petya', age: 19},
+        {id: 4, name: 'Anton', age: 37}
+    ]
     return (
         <>
             <Header headerTitle={"New Header Title"}/>
             <Body bodyTitle={"New Body Title"}/>
+            <NewComponent students={students}/>
             <Footer footerTitle={"New Footer Title"}/>
         </>
     );
